0180d24030da4e14d905903d7e83e67d290b625e
Yesung-Lim <sjsk3232@gmail.com>
2025-07-08 14:37:10 +0900

    Feat: OAuth2UserService 및 OAuth 인증 성공 핸들러 구현

---
+++ matjalalzz/src/main/java/shop/matjalalzz/global/security/oauth2/OAuth2SuccessHandler.java
@@ -0,0 +1,55 @@
+package shop.matjalalzz.global.security.oauth2;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import shop.matjalalzz.global.security.PrincipalUser;
+import shop.matjalalzz.global.security.jwt.app.TokenService;
+import shop.matjalalzz.global.security.jwt.dto.LoginTokenResponseDto;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final TokenService tokenService;
+
+    @Value("${custom.jwt.redirect-login-success}")
+    private String redirectSuccess;
+
+    @Value("${custom.jwt.token-validity-time.refresh}")
+    private int refreshTokenValidityTime;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        PrincipalUser userInfo = (PrincipalUser) authentication.getPrincipal();
+
+        LoginTokenResponseDto dto = tokenService.oauthLogin(userInfo.getUsername());
+
+        response.setHeader("Authorization", "Bearer " + dto.accessToken());
+        Cookie cookie = new Cookie("refreshToken", dto.refreshToken());
+        cookie.setHttpOnly(true);
+        //cookie.setSecure(true); // HTTPS 환경
+        cookie.setSecure(false); // HTTP 환경
+        cookie.setPath("/");
+        cookie.setMaxAge(refreshTokenValidityTime); // 7일
+        response.addCookie(cookie);
+
+//        // 프론트엔드로 리다이렉트 (토큰 포함)
+//        String targetUrl = UriComponentsBuilder.fromUriString(redirectSuccess)
+//                .build()
+//                .encode()
+//                .toUriString();
+//
+//        response.sendRedirect(targetUrl);
+    }
+}
---
+++ matjalalzz/src/main/java/shop/matjalalzz/global/security/oauth2/app/OAuth2UserService.java
@@ -0,0 +1,71 @@
+package shop.matjalalzz.global.security.oauth2.app;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import shop.matjalalzz.global.exception.BusinessException;
+import shop.matjalalzz.global.exception.domain.ErrorCode;
+import shop.matjalalzz.global.security.PrincipalUser;
+import shop.matjalalzz.global.security.oauth2.dto.GoogleResponseDto;
+import shop.matjalalzz.global.security.oauth2.dto.KakaoResponseDto;
+import shop.matjalalzz.global.security.oauth2.dto.NaverResponseDto;
+import shop.matjalalzz.global.security.oauth2.dto.OAuth2ResponseDto;
+import shop.matjalalzz.global.security.oauth2.utils.OAuth2Mapper;
+import shop.matjalalzz.user.dao.UserRepository;
+import shop.matjalalzz.user.domain.User;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public PrincipalUser loadUser(OAuth2UserRequest userRequest) {
+        // 기본 정보 가져오기
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+
+        OAuth2ResponseDto oAuth2ResponseDto = null;
+
+        switch (provider) {
+            case "google":
+                oAuth2ResponseDto = new GoogleResponseDto(oAuth2User.getAttributes());
+                break;
+            case "naver":
+                oAuth2ResponseDto = new NaverResponseDto(oAuth2User.getAttributes());
+                break;
+            case "kakao":
+                oAuth2ResponseDto = new KakaoResponseDto(oAuth2User.getAttributes());
+                break;
+            default:
+                throw new BusinessException(ErrorCode.INVALID_PROVIDER);
+        }
+
+        // providerId로 사용자 찾기
+        String oauthId = oAuth2ResponseDto.getProvider() + "_" + oAuth2ResponseDto.getProviderId();
+        Optional<User> found = userRepository.findByOauthId(oauthId);
+        if (found.isPresent()) {
+            return OAuth2Mapper.toPrincipalUser(found.get());
+        }
+
+        String email = oAuth2ResponseDto.getEmail();
+        userRepository.findByEmail(email).ifPresent(user -> {
+            throw new BusinessException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        });
+
+        User newUser = userRepository.save(OAuth2Mapper.toUser(oAuth2ResponseDto));
+
+        return OAuth2Mapper.toPrincipalUser(newUser);
+    }
+
+}
