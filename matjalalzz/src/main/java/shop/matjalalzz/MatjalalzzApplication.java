a65de8d20cdde52f8aad71ccff0dc65727c446cb
Yesung-Lim <sjsk3232@gmail.com>
2025-07-08 15:36:36 +0900

    Feat: Jpa auditing 설정 위치 변경

--- matjalalzz/src/main/java/shop/matjalalzz/MatjalalzzApplication.java
+++ matjalalzz/src/main/java/shop/matjalalzz/MatjalalzzApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @ConfigurationPropertiesScan
 @EnableFeignClients(basePackages = "shop.matjalalzz.tosspay") // TossApiClient 있는 패키지
 public class MatjalalzzApplication {
