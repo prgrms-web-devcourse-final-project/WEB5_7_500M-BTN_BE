1d85b206520a4ea613a8a9b67532ab91071551e9
msmsm0320 <ms000320@naver.com>
2025-07-08 16:09:02 +0900

    Feat: Reservation Entity 생성

---
+++ matjalalzz/src/main/java/shop/matjalalzz/reservation/entity/Reservation.java
@@ -0,0 +1,56 @@
+package shop.matjalalzz.reservation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import shop.matjalalzz.global.unit.BaseEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Reservation extends BaseEntity {
+
+    @Id
+    @Column(name = "reservation_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long clientId;
+
+    @Column(nullable = false)
+    private Long shopId;
+
+    private Long partyId;
+
+    @Column(nullable = false)
+    private int headCount;
+
+    @Column(nullable = false)
+    private int reservationFee;
+
+    @Column(nullable = false)
+    private LocalDateTime reservedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ReservationStatus status = ReservationStatus.PENDING;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+
+}
