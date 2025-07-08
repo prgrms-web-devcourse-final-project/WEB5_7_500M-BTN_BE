1d4603b8f276970f44bfa99f38286e1a674a4e16
KEEKE132 <dksvlfdhs1@gmail.com>
2025-07-08 16:19:32 +0900

    feat: 댓글 외래키 인덱스 추가

--- matjalalzz/src/main/java/shop/matjalalzz/domain/comment/entity/Comment.java
+++ matjalalzz/src/main/java/shop/matjalalzz/domain/comment/entity/Comment.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -25,6 +27,12 @@ import shop.matjalalzz.global.unit.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor
+@Table(
+    indexes = {
+        @Index(name = "idx_comment_writer", columnList = "writer_id"),
+        @Index(name = "idx_comment_party", columnList = "party_id")
+    }
+)
 public class Comment extends BaseEntity {
 
     @Id
