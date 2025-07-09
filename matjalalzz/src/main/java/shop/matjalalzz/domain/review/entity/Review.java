a449c7ed7eea6c4d865d148c4b6abfd13258b7c4
AhnPilOn <88488962+KEEKE132@users.noreply.github.com>
2025-07-09 17:26:37 +0900

    feat: 댓글, 리뷰관련 Swagger 문서화 (#8)
    * chore: 개발 초기 세팅
    
    * feat: add BaseEntity Getter
    
    * feat: Comment Swagger 문서화
    
    * feat: Review Swagger 문서화
    
    * chore: 오타수정
    
    * chore: application 설정 수정
    
    * feat: mapper private 생성자 lombok화

---
+++ matjalalzz/src/main/java/shop/matjalalzz/domain/review/entity/Review.java
@@ -0,0 +1,64 @@
+package shop.matjalalzz.domain.review.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Digits;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.matjalalzz.domain.mock.MockImage;
+import shop.matjalalzz.domain.mock.MockReservation;
+import shop.matjalalzz.domain.mock.MockShop;
+import shop.matjalalzz.domain.mock.MockUser;
+import shop.matjalalzz.global.unit.BaseEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @Column(name = "review_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 65_535, nullable = false)
+    @Lob
+    private String content;
+
+    @Column(nullable = false)
+    @Digits(integer = 1, fraction = 2)
+    private Double rating = 0D;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private MockShop shop;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private MockReservation reservation;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private MockUser writer;
+
+    @OneToMany(mappedBy = "review")
+    private List<MockImage> images;
+
+
+}
