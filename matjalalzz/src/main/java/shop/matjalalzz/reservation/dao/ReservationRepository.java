4de518f1ecda237000a60612c299566d71fd85bb
msmsm0320 <ms000320@naver.com>
2025-07-11 10:10:25 +0900

    Fix: ReservationRepository shopId 조건 수정

--- matjalalzz/src/main/java/shop/matjalalzz/reservation/dao/ReservationRepository.java
+++ matjalalzz/src/main/java/shop/matjalalzz/reservation/dao/ReservationRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import shop.matjalalzz.reservation.dto.CreateReservationRequest;
 import shop.matjalalzz.reservation.entity.Reservation;
 import shop.matjalalzz.reservation.entity.ReservationStatus;
 
@@ -12,8 +13,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     @Query("""
         SELECT r FROM Reservation r
-        WHERE r.shopId = :shopId
-          AND r.deleted = false
+        WHERE r.shop.id = :shopId
           AND (:status IS NULL OR r.status = :status)
           AND (:cursor IS NULL OR r.id < :cursor)
         ORDER BY r.id DESC
@@ -23,4 +23,11 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
         @Param("status") ReservationStatus status,
         @Param("cursor") Long cursor
     );
+
+    @Query("""
+        INSERT 
+        INTO Reservation r
+        VALUES()
+        """)
+    void createReservation(CreateReservationRequest createReservationRequest);
 }
