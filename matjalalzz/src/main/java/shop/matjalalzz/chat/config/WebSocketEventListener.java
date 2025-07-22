8d135406d212b6766b5d5f785f7e096b0ec9e899
KEEKE132 <dksvlfdhs1@gmail.com>
2025-07-22 18:36:59 +0900

    fix: 캐스팅 수정

--- matjalalzz/src/main/java/shop/matjalalzz/chat/config/WebSocketEventListener.java
+++ matjalalzz/src/main/java/shop/matjalalzz/chat/config/WebSocketEventListener.java
@@ -3,23 +3,20 @@ package shop.matjalalzz.chat.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import shop.matjalalzz.global.security.PrincipalUser;
+import shop.matjalalzz.chat.dto.StompPrincipal;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class WebSocketEventListener {
 
-    private final SimpMessagingTemplate messagingTemplate;
-
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
         log.info("Connected to websocket");
-        PrincipalUser principalUser = (PrincipalUser) event.getUser();
+        StompPrincipal principalUser = (StompPrincipal) event.getUser();
 
         if (principalUser != null) {
             log.info("Connect User: {}", principalUser.getId());
@@ -29,7 +26,7 @@ public class WebSocketEventListener {
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         log.info("Disconnected from websocket");
-        PrincipalUser principalUser = (PrincipalUser) event.getUser();
+        StompPrincipal principalUser = (StompPrincipal) event.getUser();
 
         if (principalUser != null) {
             log.info("Disconnect User: {}", principalUser.getId());
