eebe417645a18aa89f938efe36fd319158697059
Yesung-Lim <sjsk3232@gmail.com>
2025-07-24 09:49:29 +0900

    Chore: retry 설정 추가

--- matjalalzz/src/main/java/shop/matjalalzz/global/config/JpaConfig.java
+++ matjalalzz/src/main/java/shop/matjalalzz/global/config/JpaConfig.java
@@ -2,8 +2,10 @@ package shop.matjalalzz.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
+@EnableRetry
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class JpaConfig {
 
