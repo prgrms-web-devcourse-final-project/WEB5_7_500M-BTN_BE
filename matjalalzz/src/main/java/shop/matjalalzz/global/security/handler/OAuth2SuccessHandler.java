b5eae36d7f9a9c597c0184c7f2a3d401dfd949bd
Yesung-Lim <sjsk3232@gmail.com>
2025-07-27 00:44:47 +0900

    Refactor: OAuth 성공 리다이렉션 로직 수정

--- matjalalzz/src/main/java/shop/matjalalzz/global/security/handler/OAuth2SuccessHandler.java
+++ matjalalzz/src/main/java/shop/matjalalzz/global/security/handler/OAuth2SuccessHandler.java
@@ -5,15 +5,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 import shop.matjalalzz.global.security.PrincipalUser;
 import shop.matjalalzz.global.security.jwt.app.TokenService;
-import shop.matjalalzz.global.security.jwt.dto.LoginTokenResponseDto;
-import shop.matjalalzz.global.util.CookieUtils;
 
 @Slf4j
 @Component
@@ -22,25 +19,19 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final TokenService tokenService;
 
-    @Value("${custom.jwt.redirect-login-success}")
-    private String redirectSuccess;
-
-    @Value("${custom.jwt.token-validity-time.refresh}")
-    private int refreshTokenValidityTime;
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException {
         PrincipalUser userInfo = (PrincipalUser) authentication.getPrincipal();
 
-        LoginTokenResponseDto dto = tokenService.oauthLogin(userInfo.getUsername());
+        String accessToken = tokenService.oauthLogin(userInfo.getUsername());
 
-        String target = UriComponentsBuilder.fromUriString(redirectSuccess)
-            .queryParam("accessToken", dto.accessToken())
-            .build()
-            .toUriString();
+        String target = UriComponentsBuilder
+            .fromPath("/users/set-cookie")
+            .queryParam("accessToken", accessToken)
+                .build(true)
+                    .toUriString();
 
         response.sendRedirect(target);
-        CookieUtils.setRefreshTokenCookie(response, dto.refreshToken(), refreshTokenValidityTime);
     }
 }
