9fd480778fde3e326634b5ca13f6eb5acb016260
zie-ning <jeeun03@gmail.com>
2025-07-30 22:18:09 +0900

    Fix: orderSaveRequest 명세 수정

--- matjalalzz/src/main/java/shop/matjalalzz/tosspay/dto/OrderSaveRequest.java
+++ matjalalzz/src/main/java/shop/matjalalzz/tosspay/dto/OrderSaveRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record OrderSaveRequest(
     @NotBlank @Schema(description = "클라이언트에서 생성한 주문 id") String orderId,
-    @Min(value=1000) @Schema(name = "결제 금액", description = "결제 금액은 포인트 충전 머니입니다.. 1000원부터 1000원 단위로 50000원까지 충전 가능합니다.") int amount
+    @Min(value=1000) @Schema(description = "결제 금액은 포인트 충전 머니입니다.. 1000원부터 1000원 단위로 50000원까지 충전 가능합니다.") int amount
 ) {
 
 }
