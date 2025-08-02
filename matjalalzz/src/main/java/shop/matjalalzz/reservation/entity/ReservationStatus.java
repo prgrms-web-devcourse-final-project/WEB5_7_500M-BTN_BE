cad69792e42e81c3c4616ae7f73be1812ff19675
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-08-02 20:31:46 +0900

    Fix: ReservationStatus에 Swagger 적용 (#133)

--- matjalalzz/src/main/java/shop/matjalalzz/reservation/entity/ReservationStatus.java
+++ matjalalzz/src/main/java/shop/matjalalzz/reservation/entity/ReservationStatus.java
@@ -1,9 +1,23 @@
 package shop.matjalalzz.reservation.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "예약 상태 값")
 public enum ReservationStatus {
-    PENDING, // 예약 생성됨(대기 상태)
-    CANCELLED, // 예약 취소됨(예약자가 취소한 경우)
-    CONFIRMED,// 에약 승인됨(사장이 예약을 승인한 경우)
-    REFUSED,// 예약 거절됨(사장이 예약을 거절한 경우)
-    TERMINATED,// 예약이 정상적으로 종료됨
+
+    @Schema(description = "예약 생성됨(대기 상태)")
+    PENDING,
+
+    @Schema(description = "예약자가 취소함")
+    CANCELLED,
+
+    @Schema(description = "사장이 예약을 승인함")
+    CONFIRMED,
+
+    @Schema(description = "사장이 예약을 거절함")
+    REFUSED,
+
+    @Schema(description = "예약이 정상적으로 종료됨")
+    TERMINATED
 }
+
