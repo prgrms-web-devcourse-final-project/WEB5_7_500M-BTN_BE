97fd2075224b97a2c82af3958c0295eea6fa47dc
KEEKE132 <dksvlfdhs1@gmail.com>
2025-08-25 16:47:57 +0900

    refactor: Comment Facade 적용

--- matjalalzz/src/main/java/shop/matjalalzz/comment/api/CommentController.java
+++ matjalalzz/src/main/java/shop/matjalalzz/comment/api/CommentController.java
@@ -7,8 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import shop.matjalalzz.comment.app.CommentCommandService;
-import shop.matjalalzz.comment.app.CommentQueryService;
+import shop.matjalalzz.comment.app.CommentFacade;
 import shop.matjalalzz.comment.dto.CommentCreateRequest;
 import shop.matjalalzz.comment.dto.CommentResponse;
 import shop.matjalalzz.global.common.BaseResponse;
@@ -19,19 +18,18 @@ import shop.matjalalzz.global.security.PrincipalUser;
 @RequiredArgsConstructor
 public class CommentController implements CommentControllerSpec {
 
-    private final CommentQueryService commentQueryService;
-    private final CommentCommandService commentCommandService;
+    private final CommentFacade commentFacade;
 
     @Override
     public BaseResponse<List<CommentResponse>> getComments(@PathVariable Long partyId) {
-        return BaseResponse.ok(commentQueryService.findCommentsByParty(partyId), BaseStatus.OK);
+        return BaseResponse.ok(commentFacade.findCommentsByParty(partyId), BaseStatus.OK);
     }
 
     @Override
     public BaseResponse<Void> createComment(
         @PathVariable Long partyId, @Valid @RequestBody CommentCreateRequest request,
         @AuthenticationPrincipal PrincipalUser principal) {
-        commentCommandService.createComment(request, partyId, principal.getId());
+        commentFacade.createComment(request, partyId, principal.getId());
         return BaseResponse.ok(BaseStatus.CREATED);
     }
 
@@ -39,14 +37,14 @@ public class CommentController implements CommentControllerSpec {
     public void deleteComment(
         @PathVariable Long commentId,
         @AuthenticationPrincipal PrincipalUser principal) {
-        commentCommandService.deleteComment(commentId, principal.getId());
+        commentFacade.deleteComment(commentId, principal.getId());
     }
 
     @Override
     public BaseResponse<List<CommentResponse>> getInquiryComments(@PathVariable Long inquiryId,
         @AuthenticationPrincipal PrincipalUser principal) {
         return BaseResponse.ok(
-            commentQueryService.findCommentsByInquiry(inquiryId, principal.getId()),
+            commentFacade.findCommentsByInquiry(inquiryId, principal.getId()),
             BaseStatus.OK);
     }
 
@@ -54,7 +52,7 @@ public class CommentController implements CommentControllerSpec {
     public BaseResponse<Void> createInquiryComment(
         @PathVariable Long inquiryId, @Valid @RequestBody CommentCreateRequest request,
         @AuthenticationPrincipal PrincipalUser principal) {
-        commentCommandService.createInquiryComment(request, inquiryId, principal.getId());
+        commentFacade.createInquiryComment(request, inquiryId, principal.getId());
         return BaseResponse.ok(BaseStatus.CREATED);
     }
 
--- matjalalzz/src/main/java/shop/matjalalzz/comment/app/CommentQueryService.java
+++ matjalalzz/src/main/java/shop/matjalalzz/comment/app/CommentQueryService.java
@@ -5,16 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.matjalalzz.comment.dao.CommentRepository;
-import shop.matjalalzz.comment.dto.CommentResponse;
 import shop.matjalalzz.comment.entity.Comment;
-import shop.matjalalzz.comment.mapper.CommentMapper;
 import shop.matjalalzz.global.exception.BusinessException;
 import shop.matjalalzz.global.exception.domain.ErrorCode;
-import shop.matjalalzz.inquiry.dao.InquiryRepository;
-import shop.matjalalzz.inquiry.entity.Inquiry;
-import shop.matjalalzz.user.app.UserService;
-import shop.matjalalzz.user.entity.User;
-import shop.matjalalzz.user.entity.enums.Role;
 
 @Service
 @RequiredArgsConstructor
@@ -22,46 +15,17 @@ import shop.matjalalzz.user.entity.enums.Role;
 public class CommentQueryService {
 
     private final CommentRepository commentRepository;
-    private final UserService userService;
-    private final InquiryRepository inquiryRepository;
 
     public Comment getComment(Long commentId) {
         return commentRepository.findById(commentId).orElseThrow(
             () -> new BusinessException(ErrorCode.DATA_NOT_FOUND));
     }
 
-    public CommentResponse findComment(Long commentId) {
-        Comment comment = getComment(commentId);
-        return validateMap(comment);
+    public List<Comment> findCommentsByParty(Long partyId) {
+        return commentRepository.findAllByPartyId(partyId);
     }
 
-    public List<CommentResponse> findCommentsByParty(Long partyId) {
-        List<Comment> comments = commentRepository.findAllByPartyId(partyId);
-        return comments.stream().map(this::validateMap).toList();
-    }
-
-    public List<CommentResponse> findCommentsByInquiry(Long inquiryId, Long userId) {
-        User user = userService.getUserById(userId);
-        Inquiry inquiry = inquiryRepository.findById(inquiryId)
-            .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FIND_INQUIRY));
-
-        // 관리자이거나 자신이 작성한 문의글인 경우 댓글 조회
-        adminOrWriter(user, inquiry);
-
-        List<Comment> comments = commentRepository.findAllByInquiryId(inquiryId);
-        return comments.stream().map(this::validateMap).toList();
-
-    }
-
-    private CommentResponse validateMap(Comment comment) {
-        Long parentId = comment.getParent() != null ? comment.getParent().getId() : null;
-        return CommentMapper.toCommentResponse(comment, parentId);
-    }
-
-    private void adminOrWriter(User user, Inquiry inquiry) {
-        //관리자가 아니면서 자신이 쓴 문의글도 아니면 예외 발생
-        if (user.getRole() != Role.ADMIN && !inquiry.getUser().getId().equals(user.getId())) {
-            throw new BusinessException(ErrorCode.FORBIDDEN_ACCESS);
-        }
+    public List<Comment> findCommentsByInquiry(Long inquiryId) {
+        return commentRepository.findAllByInquiryId(inquiryId);
     }
 }
